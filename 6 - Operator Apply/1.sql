8329bdcb6ce4c60d29dd5f711d65016f36a134b9
Mikołaj Młodzikowski <14093153+mkjmdski@users.noreply.github.com>
2025-11-16 12:41:33 +0100

    Update 1.sql

--- 6 - Operator Apply/1.sql
+++ 6 - Operator Apply/1.sql
@@ -5,21 +5,42 @@ CROSS APPLY (
 	FROM Suppliers Sup
 	WHERE Sup.SupplierID = P.SupplierID
 ) AS S
-ORDER BY ProductID
-/*
+ORDER BY ProductID;
+
+
+
 (
-	SELECT P.ProductID, P.ProductName, S.CompanyName
-	FROM Products P
-	CROSS APPLY (
-		SELECT CompanyName
-		FROM Suppliers Sup
-		WHERE Sup.SupplierID = P.SupplierID
-	) AS S
+	(
+		SELECT P.ProductID, P.ProductName, S.CompanyName
+		FROM Products P
+		CROSS APPLY (
+			SELECT CompanyName
+			FROM Suppliers Sup
+			WHERE Sup.SupplierID = P.SupplierID
+		) AS S
+	)
+	EXCEPT
+	(
+		SELECT P.ProductID, P.ProductName, S.CompanyName
+		FROM Products P JOIN Suppliers S
+		ON P.SupplierID = S.SupplierID
+	)
 )
-EXCEPT
+UNION ALL
 (
-	SELECT P.ProductID, P.ProductName, S.CompanyName
-	FROM Products P JOIN Suppliers S
-	ON P.SupplierID = S.SupplierID
+	(
+		SELECT P.ProductID, P.ProductName, S.CompanyName
+		FROM Products P JOIN Suppliers S
+		ON P.SupplierID = S.SupplierID
+	)
+	EXCEPT
+	(
+		SELECT P.ProductID, P.ProductName, S.CompanyName
+		FROM Products P
+		CROSS APPLY (
+			SELECT CompanyName
+			FROM Suppliers Sup
+			WHERE Sup.SupplierID = P.SupplierID
+		) AS S
+	)
 )
-*/
